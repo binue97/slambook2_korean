c42dfd581d740ac2799394ed14df02680c9d68c5
binue97 <ko040123@gmail.com>
2024-01-05 22:52:11 +0900

    Translate comments in ch3

--- ch3/useEigen/eigenMatrix.cpp
+++ ch3/useEigen/eigenMatrix.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 #include <ctime>
-// Eigen 核心部分
+// Eigen core components
 #include <Eigen/Core>
-// 稠密矩阵的代数运算（逆，特征值等）
+// Algebraic operations of dense matrix
 #include <Eigen/Dense>
 
 using namespace Eigen;
@@ -13,100 +13,96 @@ using namespace Eigen;
 #define MATRIX_SIZE 50
 
 /****************************
-* 本程序演示了 Eigen 基本类型的使用
+* This program demonstrates the use of Eigen basic types.
 ****************************/
 
 int main(int argc, char **argv) {
-  // Eigen 中所有向量和矩阵都是Eigen::Matrix，它是一个模板类。它的前三个参数为：数据类型，行，列
-  // 声明一个2*3的float矩阵
+  // All vectors and matrices in Eigen are Eigen::Matrix which is a template class.
+  // Matrix<DataType, Row, Col>
   Matrix<float, 2, 3> matrix_23;
 
-  // 同时，Eigen 通过 typedef 提供了许多内置类型，不过底层仍是Eigen::Matrix
-  // 例如 Vector3d 实质上是 Eigen::Matrix<double, 3, 1>，即三维向量
+  // Eigen provides many built-in types via typedef, but the base layer is still Eigen::Matrix.
+  // For example, Vector3d is essentially Eigen::Matrix<double, 3, 1>, which is a three-dimensional vector.
   Vector3d v_3d;
-  // 这是一样的
   Matrix<float, 3, 1> vd_3d;
 
-  // Matrix3d 实质上是 Eigen::Matrix<double, 3, 3>
-  Matrix3d matrix_33 = Matrix3d::Zero(); //初始化为零
-  // 如果不确定矩阵大小，可以使用动态大小的矩阵
+  // Matrix3d is essentially Eigen::Matrix<double, 3, 3>
+  Matrix3d matrix_33 = Matrix3d::Zero(); // Initialize with zero.
+  // If you are unsure of the matrix size, you can use a dynamically sized matrix.
   Matrix<double, Dynamic, Dynamic> matrix_dynamic;
-  // 更简单的
+  // Simpler form using typedef
   MatrixXd matrix_x;
-  // 这种类型还有很多，我们不一一列举
 
-  // 下面是对Eigen阵的操作
-  // 输入数据（初始化）
+  // Initialization using operator overloading
   matrix_23 << 1, 2, 3, 4, 5, 6;
-  // 输出
   cout << "matrix 2x3 from 1 to 6: \n" << matrix_23 << endl;
 
-  // 用()访问矩阵中的元素
+  // Access matrix element with operator ()
   cout << "print matrix 2x3: " << endl;
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 3; j++) cout << matrix_23(i, j) << "\t";
+  for (int i = 0; i < 2; i++) 
+  {
+    for (int j = 0; j < 3; j++) 
+     cout << matrix_23(i, j) << "\t";
     cout << endl;
   }
 
-  // 矩阵和向量相乘（实际上仍是矩阵和矩阵）
+  // Initialize vectors declared above.
   v_3d << 3, 2, 1;
   vd_3d << 4, 5, 6;
 
-  // 但是在Eigen里你不能混合两种不同类型的矩阵，像这样是错的
-  // Matrix<double, 2, 1> result_wrong_type = matrix_23 * v_3d;
-  // 应该显式转换
+  // When operating with matrix and vectors, their data type should match.
+  // Matrix<double, 2, 1> result_wrong_type = matrix_23 * v_3d; <-- double and float can't be operated together.
+  // So, user must explicitly cast their data types.
   Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
   cout << "[1,2,3;4,5,6]*[3,2,1]=" << result.transpose() << endl;
 
   Matrix<float, 2, 1> result2 = matrix_23 * vd_3d;
   cout << "[1,2,3;4,5,6]*[4,5,6]: " << result2.transpose() << endl;
 
-  // 同样你不能搞错矩阵的维度
-  // 试着取消下面的注释，看看Eigen会报什么错
+  // When multiplying matrices, user should be aware of their dimensions.
+  // Wrong dimensions will trigger error.
   // Eigen::Matrix<double, 2, 3> result_wrong_dimension = matrix_23.cast<double>() * v_3d;
 
-  // 一些矩阵运算
-  // 四则运算就不演示了，直接用+-*/即可。
-  matrix_33 = Matrix3d::Random();      // 随机数矩阵
+  // Some matrix operations
+  matrix_33 = Matrix3d::Random();
   cout << "random matrix: \n" << matrix_33 << endl;
-  cout << "transpose: \n" << matrix_33.transpose() << endl;      // 转置
-  cout << "sum: " << matrix_33.sum() << endl;            // 各元素和
-  cout << "trace: " << matrix_33.trace() << endl;          // 迹
-  cout << "times 10: \n" << 10 * matrix_33 << endl;               // 数乘
-  cout << "inverse: \n" << matrix_33.inverse() << endl;        // 逆
-  cout << "det: " << matrix_33.determinant() << endl;    // 行列式
+  cout << "transpose: \n" << matrix_33.transpose() << endl;
+  cout << "sum: " << matrix_33.sum() << endl; // Sum of all elements in matrix.
+  cout << "trace: " << matrix_33.trace() << endl;
+  cout << "times 10: \n" << 10 * matrix_33 << endl;
+  cout << "inverse: \n" << matrix_33.inverse() << endl;
+  cout << "det: " << matrix_33.determinant() << endl;
 
-  // 特征值
-  // 实对称矩阵可以保证对角化成功
+  // Eigen value
+  // The real symmetric matrix can guarantee the diagonalization success.
   SelfAdjointEigenSolver<Matrix3d> eigen_solver(matrix_33.transpose() * matrix_33);
   cout << "Eigen values = \n" << eigen_solver.eigenvalues() << endl;
   cout << "Eigen vectors = \n" << eigen_solver.eigenvectors() << endl;
 
-  // 解方程
-  // 我们求解 matrix_NN * x = v_Nd 这个方程
-  // N的大小在前边的宏里定义，它由随机数生成
-  // 直接求逆自然是最直接的，但是求逆运算量大
-
+  // Solving equations
+  // matrix_NN * x = v_Nd
+  // MATRIX_SIZE is defined by macro, and matrix_NN is initialized with random value.
+  // Directly calculating inverse matrix is straight-forward, but it is a lot of computation.
   Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN
       = MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
-  matrix_NN = matrix_NN * matrix_NN.transpose();  // 保证半正定
+  matrix_NN = matrix_NN * matrix_NN.transpose();  // Semi-positive definite
   Matrix<double, MATRIX_SIZE, 1> v_Nd = MatrixXd::Random(MATRIX_SIZE, 1);
 
-  clock_t time_stt = clock(); // 计时
-  // 直接求逆
+  clock_t time_stt = clock();
+  // Direct inversion
   Matrix<double, MATRIX_SIZE, 1> x = matrix_NN.inverse() * v_Nd;
   cout << "time of normal inverse is "
        << 1000 * (clock() - time_stt) / (double) CLOCKS_PER_SEC << "ms" << endl;
   cout << "x = " << x.transpose() << endl;
 
-  // 通常用矩阵分解来求，例如QR分解，速度会快很多
+  // Usually used matrix decomposition, such as QR decomposition, will be much faster.
   time_stt = clock();
   x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
   cout << "time of Qr decomposition is "
        << 1000 * (clock() - time_stt) / (double) CLOCKS_PER_SEC << "ms" << endl;
   cout << "x = " << x.transpose() << endl;
 
-  // 对于正定矩阵，还可以用cholesky分解来解方程
+  // For positive definite matrices, the equation can also be solved by Cholesky decomposition.
   time_stt = clock();
   x = matrix_NN.ldlt().solve(v_Nd);
   cout << "time of ldlt decomposition is "
